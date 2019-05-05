e3fe014ab511845b3f5b5c1cbdebf9e4e46699b3
zombiej <smith3816@gmail.com>
2019-05-05 12:17:20 +0800

    init

---
+++ examples/basic.js
@@ -0,0 +1,13 @@
+/* eslint no-console: 0 */
+
+import React from 'react';
+// import Mentions from '../src';
+import '../assets/index.less';
+
+class Demo extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+export default Demo;
---
+++ src/Mentions.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default class Mentions extends React.Component {
+  render() {
+    return null;
+  }
+}
