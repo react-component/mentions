2be2e3bcd1e2a65b00916a7f4bf411c6367eb14b
zombiej <smith3816@gmail.com>
2019-05-06 15:07:27 +0800

    clean up

--- examples/basic.js
+++ examples/basic.js
@@ -17,7 +17,6 @@ class Demo extends React.Component {
           <Option value="bamboo">Bamboo</Option>
           <Option value="cat">Cat</Option>
         </Mentions>
-        {/* <textarea className="ttt" /> */}
       </div>
     );
   }
