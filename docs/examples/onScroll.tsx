ae96b1f9e94865b46d6e164cb90f3f13915b37aa
二货机器人 <smith3816@gmail.com>
2025-02-28 11:55:34 +0800

    docs: fix lint

--- docs/examples/onScroll.tsx
+++ docs/examples/onScroll.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import Mentions from '@rc-component/mentions';
+import Mentions, { UnstableContext } from '@rc-component/mentions';
 import '../../assets/index.less';
 import './onScroll.less';
 
 export default () => (
-  <Mentions
-    rows={3}
-    defaultValue="Hello @ World @"
-    onPopupScroll={console.log}
-    popupClassName="on-scroll"
-    open
-    options={Array.from({ length: 1000 }).map((_, index) => ({
-      value: `item-${index}`,
-      label: `item-${index}`,
-    }))}
-  />
+  <UnstableContext.Provider value={{ open: true }}>
+    <Mentions
+      rows={3}
+      defaultValue="Hello @ World @"
+      onPopupScroll={console.log}
+      popupClassName="on-scroll"
+      options={Array.from({ length: 1000 }).map((_, index) => ({
+        value: `item-${index}`,
+        label: `item-${index}`,
+      }))}
+    />
+  </UnstableContext.Provider>
 );
