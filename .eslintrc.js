c257bd1ff8bbcb25286d35723e816624440bac96
MadCcc <1075746765@qq.com>
2025-04-03 18:20:42 +0800

    chore: bump @rc-component/menu to 1.1.0 (#293)
    * chore: bump @rc-component/menu to 1.1.0
    
    * chore: fix eslint
    
    * chore: update
    
    * chore: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
   rules: {
     'jsx-a11y/no-autofocus': 0,
+    '@typescript-eslint/consistent-indexed-object-style': 0,
+    '@typescript-eslint/no-parameter-properties': 0,
+    '@typescript-eslint/no-throw-literal': 0,
+    '@typescript-eslint/type-annotation-spacing': 0,
+    '@typescript-eslint/ban-types': 0,
   },
   overrides: [
     {
